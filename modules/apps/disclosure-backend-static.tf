a9ea9d09727825f9a047e70d94caf73c99e6b2a8
Tom Dooner <tom.dooner@brigade.com>
2017-04-08 13:32:51 -0700

    Remove health check of disclosure-backend-static
    It's costing me $2.75/mo. What a gigantic waste of money! /s

--- modules/apps/disclosure-backend-static.tf
+++ modules/apps/disclosure-backend-static.tf
@@ -5,12 +5,3 @@ resource "cloudflare_record" "disclosure-backend-static" {
   type = "CNAME"
   proxied = true
 }
-
-module "disclosure_backend_status_check" {
-  source = "../aws-status-check"
-
-  fqdn = "disclosure-backend-static.tdooner.com"
-  type = "HTTPS"
-  port = 443
-  sns_arn = "arn:aws:sns:us-east-1:428663493796:downtime-alerts"
-}
